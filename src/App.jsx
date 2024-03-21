a373193ee9198037ec810987ac34fe52c75ed53e
juagilei <juagilei@gmail.com>
2024-03-21 20:37:15 +0100

    actualizacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,36 @@
 import { useEffect, useState } from "react";
-export default function App() {
-const [moveText, setMoveText] = useState(false);
 
-  // Este efecto se ejecutará al cargar el componente y moverá el texto después de 1 segundo
+export default function App() {
+  const [moveText, setMoveText] = useState(false);
+
   useEffect(() => {
+    // Establece el estado para iniciar la animación después de un retraso
     const timeout = setTimeout(() => {
       setMoveText(true);
-    }, 10);
+    }, 1000); // Cambiado a 1000 para que la animación comience después de 1 segundo
 
     // Limpia el temporizador al desmontar el componente para evitar fugas de memoria
     return () => clearTimeout(timeout);
   }, []);
 
   return (
-    <div className="h-screen flex items-center justify-center">
-      <section className='text-2xl'>
-      <h1 className="flex justify-center"> Enlace </h1>
-      <h1 className="flex justify-center">Elena</h1>
-      <h1 className="flex justify-center">&</h1>
-      <h1 className="flex justify-center"> Juan Antonio</h1>
+    <>
+      <div className="relative bg-amber-200  h-screen">
+      <img src="../public/invitacion boda.png" alt="tarjeta" />
+
+      <section
+        className={`absolute top-20 left-0 ${moveText ? "translate-x-11" : "-translate-x-full"} transition-transform duration-1000`}
+      >
+        <img src="../public/texto principal.png" alt="texto" width={300} />
       </section>
     </div>
+      <div className="relative bg-amber-200  h-screen">
+        <img src="../public/invitacion boda.png" alt="tarjeta" />
+      </div>
+    
+      <div className="relative bg-amber-200  h-screen">
+        <img src="../public/invitacion boda.png" alt="tarjeta" />
+    </div>
+    </>
   );
 }
